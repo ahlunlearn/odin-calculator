9ac80e70b619f91dd2b8f2d9707e265dceeab500
luan <lunchiuah@gmail.com>
2022-08-20 22:47:27 +0800

    click number button can display on the tp[

--- script.js
+++ script.js
@@ -30,3 +30,11 @@ function operate(cal,a,b){
             break
     }
 }
+let input_str = ''
+const display = document.querySelector('.display input')
+const num_btns = document.querySelectorAll('.num_btns button')
+for(let i = 0; i < num_btns.length;i++){
+    num_btns[i].addEventListener('click', ()=>{
+        display.value += num_btns[i].value
+    })
+}
