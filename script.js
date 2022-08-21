5b200725bbeb84afa6b6bcda423367e128c2f8ee
luan <lunchiuah@gmail.com>
2022-08-21 17:49:46 +0800

    rework on the UI and finish fundamental functions

--- script.js
+++ script.js
@@ -32,7 +32,12 @@ function operate(cal,a,b){
 }
 let input_str = ''
 const display = document.querySelector('.display')
-const num_btns = document.querySelectorAll('.num_btns button')
+let num1 = 0
+let num2 = 0
+let result = 0
+let operator = ''
+// number button function
+const num_btns = document.querySelectorAll('.num')
 for(let i = 0; i < num_btns.length;i++){
     num_btns[i].addEventListener('click', ()=>{
         if(display.innerText === '0'){
@@ -40,5 +45,41 @@ for(let i = 0; i < num_btns.length;i++){
         } else {
             display.innerText += num_btns[i].value
         }
+        if(operator === ''){
+            num1 = parseFloat(display.innerText)
+        } else {
+            num2 = parseFloat(display.innerText)
+        }
     })
 }
+// clear button function
+const clear_btn = document.querySelector('.clear')
+clear_btn.addEventListener('click',()=>{
+    display.innerText = '0'
+})
+// delete button function
+const delete_btn = document.querySelector('.delete')
+delete_btn.addEventListener('click',()=>{
+    display.innerText = display.innerText.slice(0,-1)
+    if(display.innerText === ''){
+        display.innerText = '0'
+    }
+})
+// operator button function
+const operator_btns = document.querySelectorAll('.operator')
+for(let i = 0; i < operator_btns.length;i++){
+    operator_btns[i].addEventListener('click', ()=>{
+        operator = operator_btns[i].value
+        num1 = parseFloat(display.innerText)
+        display.innerText = ''
+    })
+}
+// equal button function
+const equal_btn = document.querySelector('.equal')
+equal_btn.addEventListener('click',()=>{
+    if(num1 !== '' && operator !== '' && display.innerText !== ''){
+        num2 = parseFloat(display.innerText)
+        result = operate(operator,num1,num2)
+        display.innerText = result
+    }
+})
