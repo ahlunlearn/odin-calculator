c798cad68aa800cc9999a9f1e9c6cafb8bf06e62
luan <lunchiuah@gmail.com>
2022-08-21 21:05:26 +0800

    make operator calculate

--- script.js
+++ script.js
@@ -36,10 +36,14 @@ let num1 = 0
 let num2 = 0
 let result = 0
 let operator = ''
+let isoperatorpressed = false
 // number button function
 const num_btns = document.querySelectorAll('.num')
 for(let i = 0; i < num_btns.length;i++){
     num_btns[i].addEventListener('click', ()=>{
+        if(isoperatorpressed){
+            display.innerText = ''
+        }
         if(display.innerText === '0'){
             display.innerText = num_btns[i].value
         } else {
@@ -50,12 +54,39 @@ for(let i = 0; i < num_btns.length;i++){
         } else {
             num2 = parseFloat(display.innerText)
         }
+        isoperatorpressed = false
     })
 }
+
+const dot_btn = document.querySelector('.dot')
+dot_btn.addEventListener('click', ()=>{
+    if(display.innerText.includes('.')){
+        return
+    }
+    if(isoperatorpressed){
+        display.innerText = ''
+    }
+    if(display.innerText === '0'){
+        display.innerText = dot_btn.value
+    } else {
+        display.innerText += dot_btn.value
+    }
+    if(operator === ''){
+        num1 = parseFloat(display.innerText)
+    } else {
+        num2 = parseFloat(display.innerText)
+    }
+    isoperatorpressed = false
+})
 // clear button function
 const clear_btn = document.querySelector('.clear')
 clear_btn.addEventListener('click',()=>{
     display.innerText = '0'
+    operator = ''
+    num1 = 0
+    num2 = 0
+    result = 0
+    isoperatorpressed = false
 })
 // delete button function
 const delete_btn = document.querySelector('.delete')
@@ -69,17 +100,30 @@ delete_btn.addEventListener('click',()=>{
 const operator_btns = document.querySelectorAll('.operator')
 for(let i = 0; i < operator_btns.length;i++){
     operator_btns[i].addEventListener('click', ()=>{
+        if(display.innerText !== '' && operator !== '' && !isoperatorpressed){
+            calculate()
+        } else {
+            num1 = parseFloat(display.innerText)
+        }
         operator = operator_btns[i].value
-        num1 = parseFloat(display.innerText)
-        display.innerText = ''
+        isoperatorpressed = true
     })
 }
+
+
 // equal button function
 const equal_btn = document.querySelector('.equal')
 equal_btn.addEventListener('click',()=>{
     if(num1 !== '' && operator !== '' && display.innerText !== ''){
-        num2 = parseFloat(display.innerText)
-        result = operate(operator,num1,num2)
-        display.innerText = result
+        calculate()
     }
 })
+
+function calculate(){
+    // num2 = parseFloat(display.innerText)
+    result = operate(operator,num1,num2)
+    display.innerText = result
+    num1 = result
+    operator = ''
+    num2 = 0
+}
